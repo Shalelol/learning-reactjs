bfd6e08bcd38d39a0fd57d1cbee8228881bff7ba
Shale Kuzmanovski <Shale.lol@hotmail.com>
2017-01-03 22:15:26 +1100

    Managing react component state with setState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
 const App = React.createClass({
+  getInitialState(){
+    return {
+      text: 'this is the state text',
+      cat: 0
+    }
+  },
+  update(e){
+    this.setState({
+      text: e.target.value
+    })
+  },
   render(){
     let text = this.props.text;
     return (
       <div>
-        <h1>{text}</h1>
+        <input type="text" onChange={this.update.bind(this)} />
+        <h1>{this.state.text} - {this.state.cat}</h1>
       </div>
     )
   },
